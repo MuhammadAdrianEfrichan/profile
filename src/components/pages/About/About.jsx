fd4449570339d7254d125896e26b9883ec2ff68d
MuhammadAdrianEfrichan <adrianefrich@gmail.com>
2026-05-20 20:57:45 +0700

    fix bug

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,6 +1,6 @@
 const About = ()=>{
     return <div className="container mx-auto px-6 my-40 lg:flex lg:justify-around ">
-        <div className=" h-70 border lg:w-110 xl:w-130 "></div>
+        <div className="h-70 border lg:w-110 xl:w-130 "></div>
         <div className="w-100% h-auto  lg:w-110 mt-10 lg:mt-0">
             <div className="text-3xl font-serif font-bold text-center pt-10 pb-5 text-blue-600 lg:pt-0 ">About Us</div>
                 <p className="indent-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor animi voluptates vitae soluta ipsa consequuntur sed deserunt impedit. Reiciendis ab dicta quisquam velit est aut quibusdam laboriosam unde, quas molestiae. Quos excepturi aliquam, mollitia alias quaerat ut ipsum totam obcaecati commodi soluta laborum quod molestiae recusandae esse expedita. Quibusdam, id.</p>
--- src/components/pages/Hero/Hero.jsx
+++ src/components/pages/Hero/Hero.jsx
@@ -1,11 +1,11 @@
 import image from "../../../assets/hero/hero.jpeg"
 const Hero = ()=>{
-  return <div className="container mx-auto">
+  return <div className="mx-auto">
         <div className="w-full h-230 border">
           <div className="w-full h-full bg-black relative"><img src={image} alt="Hero" className="h-full w-full object-cover opacity-30" />
-          <div className="px-6 absolute top-70 text-white text-[70px] font-serif text-base/20"><div className="text-[50px]">HMJ-IF</div>WE ARE <div className="text-[90px] text-blue-400 ">FAMILY.</div>
+          <div className="pl-6 absolute top-70 text-white text-[70px] font-serif text-base/20"><div className="text-[50px]">HMJ-IF</div>WE ARE <div className="text-[80px] text-blue-400 ">FAMILY.</div>
             </div>
-            <p className="px-6 w-100 absolute top-150 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem assumenda enim dolorum deleniti obcaecati nulla laboriosam sit qui quia molestiae.</p>
+            <p className="px-6 w-full max-w-md absolute top-140 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem assumenda enim dolorum deleniti obcaecati nulla laboriosam sit qui quia molestiae.</p>
           </div>
         
         </div>
